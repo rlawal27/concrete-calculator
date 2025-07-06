ebc096ac2da07ca0cf93aa3ee12fb6f35bfe0d45
Abdulrasheed Lawal <rlawal27@gmail.com>
2025-07-06 23:14:04 +0100

    Fix: bind to 0.0.0.0 and use Render port

--- app.py
+++ app.py
@@ -34,5 +34,9 @@ def calculate_volume():
 
     return render_template('index.html', result=result, materials=materials)
 
+import os
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port)
+
